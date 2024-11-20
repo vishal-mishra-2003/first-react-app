1df865f3a3652d850ce0aa8dcae2fe32b8144c89
vishal-mishra-2003 <vm3446284@gmail.com>
2024-11-20 15:34:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+
+import "./styles/App.css";
+import "./styles/header.css";
+import "./styles/home.css";
+import "./styles/footer.css";
+import "./styles/contact.css";
+import "./styles/mediaquery.css";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
